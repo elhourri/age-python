8899ebf2918ce586f263fba3405890dddb5c62e4
elhourri <146944553+elhourri@users.noreply.github.com>
2023-10-09 22:19:34 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+a = int(input("Entrez l'âge de l'enfant : "))
+
+if a >= 6 and a <= 7:
+    print("L'enfant est un poussin.")
+elif a >= 8 and a <= 9:
+    print("L'enfant est un pupille.")
+elif a >= 10 and a <= 11:
+    print("L'enfant est un minime.")
+elif a >= 12:
+    print("L'enfant est un cadet.")
